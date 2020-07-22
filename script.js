3486f3bb9cca6edb8cbaa04994733f16457091d1
Alexandr Krylov <alexandr.krylov@gmail.com>
2020-07-22 16:03:52 +0600

    bonus 4 completed. see readme.md

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ const canvas = document.getElementById("canvas");
 const color  =  document.querySelector("input[type=color]");
 const saveButton = document.getElementById("save-button");
 const loadButton = document.getElementById("load-button");
+const drawButton = document.getElementById("pencil-button");
+const fillingButton = document.getElementById("filling-button");
 const storage = window.localStorage;
 saveButton.addEventListener("click", saveCanvas);
 loadButton.addEventListener("click", loadCanvas);
-
+drawButton.addEventListener("click", switchToDrawing);
+fillingButton.addEventListener("click", switchToFilling);
 color.setAttribute("data-value", color.value);
 color.addEventListener("change", customColorChange);
 canvas.addEventListener("click", drawPixel);
@@ -33,7 +36,7 @@ for (let i = 0; i < canvasHeight; i++){
 
 const palettes = document.querySelectorAll(".palette");
 palettes.forEach(function (palette) {
-    palette.style.background = palette.getAttribute("data-value");
+    palette.style.backgroundColor = palette.getAttribute("data-value");
 });
 showCurrentCollor();
 
@@ -42,7 +45,7 @@ palette.addEventListener("click", setColor);
 
 function drawPixel(evt) {
     if (evt.target.classList.contains("pixel")) {
-        evt.target.style.background = currentColor;
+        evt.target.style.backgroundColor = currentColor;
         evt.stopPropagation();
     }
 }
@@ -78,7 +81,7 @@ function draw(evt) {
 function customColorChange(evt) {
     currentColor = evt.target.value;
     evt.target.setAttribute("data-value", currentColor);
-    evt.target.style.background = currentColor;
+    evt.target.style.backgroundColor = currentColor;
     evt.stopPropagation();
     showCurrentCollor();
 }
@@ -120,5 +123,125 @@ function loadCanvas() {
             canvasForSavePixel = 0;
             canvasForSaveRow++
         }
+    }
+}
+
+function floodFill(element, replaceableColor, fillingColor) {
+    if (element.style.backgroundColor !== replaceableColor) return;
+    element.style.backgroundColor = fillingColor;
+    let nextElement = getNorthElement(element);
+    if (nextElement !== null) {
+        floodFill(nextElement, replaceableColor, fillingColor);
+    }
+    nextElement = getEastElement(element);
+    if (nextElement !== null) {
+        floodFill(nextElement, replaceableColor, fillingColor);
+    }
+    nextElement = getSouthElement(element);
+    if (nextElement !== null) {
+        floodFill(nextElement, replaceableColor, fillingColor);
+    }
+    nextElement = getWestElement(element);
+    if (nextElement !== null) {
+        floodFill(nextElement, replaceableColor, fillingColor);
+    }
+}
+
+function getNorthElement(element) {
+    let parent = element.parentElement, northRow = null;
+    while (parent.previousSibling) {
+        if (parent.previousSibling.classList.contains("row")) {
+            northRow = parent.previousSibling;
+            break;
+        }
+        parent = parent.previousSibling;
+    }
+    if (!northRow) return null;
+    let westCount = 0;
+    while (element.previousSibling) {
+        if (element.previousSibling.classList.contains("pixel")) westCount ++;
+        element = element.previousSibling;
+    }
+    let northPixelCandidate = northRow.firstChild;
+    let northPixel = null;
+    while (northPixelCandidate) {
+        if (northPixelCandidate.classList.contains("pixel")) {
+            if (westCount == 0) {
+                northPixel = northPixelCandidate;
+                break;
+            }
+            westCount --;
+        }
+        northPixelCandidate = northPixelCandidate.nextSibling;
+    }
+    return northPixel;
+}
+
+function getEastElement(element) {
+    while (element.nextSibling) {
+        if (element.nextSibling.classList.contains("pixel")) return  element.nextSibling;
+        element = element.nextSibling;
+    }
+    return null;
+}
+
+function getSouthElement(element) {
+    let parent = element.parentElement, southRow = null;
+    while (parent.nextSibling) {
+        if (parent.nextSibling.classList.contains("row")) {
+            southRow = parent.nextSibling;
+            break;
+        }
+        parent = parent.nextSibling;
+    }
+    if (!southRow) return null;
+    let westCount = 0;
+    while (element.previousSibling) {
+        if (element.previousSibling.classList.contains("pixel")) westCount ++;
+        element = element.previousSibling;
+    }
+    let southPixelCandidate = southRow.firstChild;
+    let southPixel = null;
+    while (southPixelCandidate) {
+        if (southPixelCandidate.classList.contains("pixel")) {
+            if (westCount == 0) {
+                southPixel = southPixelCandidate;
+                break;
+            }
+            westCount --;
+        }
+        southPixelCandidate = southPixelCandidate.nextSibling;
+    }
+    return southPixel;
+}
+
+function getWestElement(element) {
+    while (element.previousSibling) {
+        if  (element.previousSibling.classList.contains("pixel"))  return element.previousSibling;
+        element = element.previousSibling;
+    }
+    return null;
+}
+
+function switchToFilling() {
+    canvas.removeEventListener("click", drawPixel);
+    canvas.removeEventListener("mousedown", startDraw);
+    canvas.removeEventListener("mouseup", stopDraw);
+    canvas.addEventListener("click", filling);
+    canvas.style.cursor = "url('icons/paint_32_32.png') 30 27, default";
+}
+
+function switchToDrawing() {
+    canvas.removeEventListener("click", filling);
+    canvas.addEventListener("click",  drawPixel);
+    canvas.addEventListener("mousedown", startDraw);
+    canvas.addEventListener("mouseup", stopDraw);
+    canvas.style.cursor = "default";
+}
+
+function filling(evt) {
+    if (evt.target.classList.contains("pixel")) {
+        if (evt.target.style.backgroundColor == currentColor) return;
+        floodFill(evt.target, evt.target.style.backgroundColor, currentColor);
     }
 }
